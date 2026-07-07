8b01b0fe9d1c9ea9d9cd20a6d6db74906e6b2535
Sarthak12397 <ssarthak737@gmail.com>
2026-07-08 00:00:18 +0545

    Entites started, Config, Enums and Exception finished

--- ReconciliationFabric.Domain/Entities/ReconciliationJob.cs
+++ ReconciliationFabric.Domain/Entities/ReconciliationJob.cs
@@ -7,7 +7,12 @@ public class ReconciliationJob
         public Guid? SupersedesJobId {get; set;}
         public int Revision {get; set; }
         public ReconciliationJobType JobType{get; set;}
-
-
+       public ReconciliationWindow   Window{get;set;}  
+       public DateTime  CreatedAt{get; set;}       
+       public ReconciliationJobStatus Status{get; private set;}    
+       public int TotalRecordsProcessed{get; private set;}     
+       public int DiscrepanciesFound{get; private set;}
+       public bool CircuitBreakerTripped{get; private set;}
+       
     
 }
